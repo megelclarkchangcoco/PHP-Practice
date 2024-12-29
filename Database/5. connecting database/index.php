346e45c6baf3ccb65cc36dbf6df8226020ca66cf
megelclarkchangcoco <megelnu@gmail.com>
2024-12-30 03:18:00 +0800

    first commit

---
+++ Database/5. connecting database/index.php
@@ -0,0 +1,19 @@
+<?php 
+    // 1. MySQLi Extension
+    // 2. PDO (PHP Data Objects)
+
+    include("database.php");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    hello<br>
+    
+</body>
+</html>
